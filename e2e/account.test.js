b1c6b3935fe4a57d9484bf8a0df09ab800c0c82a
Douglas Guimarães <eu@devdouglas.com>
2023-01-25 17:06:21 -0300

    improving base test

--- e2e/account.test.js
+++ e2e/account.test.js
@@ -15,20 +15,38 @@ describe("Account tests", () => {
     await expect(element(by.id(testIds.baseTemplate.insertButton))).toBeVisible();
   });
 
-  it("should navigate to account insertion screen after tap insert button", async () => {
-    await element(by.id(testIds.baseTemplate.insertButton)).tap();
+  describe("navigation tests", () => {
+    it("should navigate to insertion screen after tap back button", async () => {
+      await element(by.id(testIds.baseTemplate.insertButton)).tap();
 
-    await expect(element(by.text("Inserir Conta"))).toBeVisible();
-    await expect(element(by.id(testIds.baseTemplate.backButton))).toBeVisible();
-    await expect(element(by.id(testIds.baseTemplate.finishButton))).toBeVisible();
+      await expect(element(by.text("Inserir Conta"))).toBeVisible();
+      await expect(element(by.id(testIds.baseTemplate.backButton))).toBeVisible();
+      await expect(element(by.id(testIds.baseTemplate.finishButton))).toBeVisible();
+
+      await element(by.id(testIds.baseTemplate.backButton)).tap();
+      await expect(element(by.text("Plano de Contas"))).toBeVisible();
+    });
+
+    device.getPlatform() === "android" &&
+      it("should come back to accountlist from insertion screen using system back button (Android)", async () => {
+        await element(by.id(testIds.baseTemplate.insertButton)).tap();
+
+        await expect(element(by.text("Inserir Conta"))).toBeVisible();
+        await expect(element(by.id(testIds.baseTemplate.backButton))).toBeVisible();
+        await expect(element(by.id(testIds.baseTemplate.finishButton))).toBeVisible();
+        await device.pressBack();
+        await expect(element(by.text("Plano de Contas"))).toBeVisible();
+      });
   });
 
-  it("should create an account with code 1 and name 'Test' (1 - Test)", async () => {
-    await element(by.id(testIds.baseTemplate.insertButton)).tap();
-    await element(by.id(testIds.account.accountCodeInput)).typeText("1");
-    await element(by.id(testIds.account.accountNameInput)).typeText("Test");
-    await element(by.id(testIds.baseTemplate.finishButton)).tap();
+  describe("account management", () => {
+    it("should create an account with code 1 and name 'Test' (1 - Test)", async () => {
+      await element(by.id(testIds.baseTemplate.insertButton)).tap();
+      await element(by.id(testIds.account.accountCodeInput)).typeText("1");
+      await element(by.id(testIds.account.accountNameInput)).typeText("Test");
+      await element(by.id(testIds.baseTemplate.finishButton)).tap();
 
-    await expect(element(by.text("1 - Test"))).toBeVisible();
+      await expect(element(by.text("1 - Test"))).toBeVisible();
+    });
   });
 });
